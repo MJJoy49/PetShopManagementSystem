911d382066dd90aa690dc7c8c7dea573239fa1e9
MJJoy49 <mjjoy546@gmail.com>
2024-12-27 23:59:11 +0600

    Welcome Form complete

---
+++ PetShopManagementSystem/FormStyle/RoundForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class RoundForm
+{
+    public static void roundedForm(Form form, int radius)
+    {
+        // Create rounded rectangle path
+        GraphicsPath path = new GraphicsPath();
+        path.AddArc(new Rectangle(0, 0, radius, radius), 180, 90);
+        path.AddArc(new Rectangle(form.Width - radius, 0, radius, radius), 270, 90);
+        path.AddArc(new Rectangle(form.Width - radius, form.Height - radius, radius, radius), 0, 90);
+        path.AddArc(new Rectangle(0, form.Height - radius, radius, radius), 90, 90);
+        path.CloseFigure();
+
+        // Set the form region to the rounded rectangle path
+        form.Region = new Region(path);
+
+        // Optional: Draw border
+        form.Paint += (sender, e) =>
+        {
+            Pen pen = new Pen(Color.Black, 3); // Adjust color and thickness as needed
+            e.Graphics.DrawPath(pen, path);
+        };
+    }
+}
---
+++ PetShopManagementSystem/views/WelcomeForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetShopManagementSystem.views
+{
+    public partial class WelcomeForm : Form
+    {
+        private Point startLocation = new Point(125, 173);
+        private Point endLocation = new Point(894, 173);
+        private Size startSize = new Size(795, 153);
+        private Size endSize = new Size(15, 153);
+        private int steps = 120; // Total steps (8 seconds / 50ms interval)
+        private int currentStep = 0;
+        private float deltaWidth;
+        private float deltaX;
+
+
+
+
+        public WelcomeForm()
+        {
+            InitializeComponent();
+
+            // Set initial size and location for AnimationPanel
+            AnimationPanel.Size = startSize;
+            AnimationPanel.Location = startLocation;
+
+            // Calculate step values for size and location
+            deltaWidth = (float)(endSize.Width - startSize.Width) / steps;
+            deltaX = (float)(endLocation.X - startLocation.X) / steps;
+            timer1.Start();
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (currentStep < steps)
+            {
+                // Update size and location incrementally
+                AnimationPanel.Size = new Size(
+                    (int)(startSize.Width + deltaWidth * currentStep),
+                    startSize.Height
+                );
+                AnimationPanel.Location = new Point(
+                    (int)(startLocation.X + deltaX * currentStep),
+                    startLocation.Y
+                );
+                currentStep++;
+            }
+            else
+            {
+                // Stop the timer when the animation is complete
+                timer1.Stop();
+                Application.Exit();
+            }
+        }
+
+        private void AnimationPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
